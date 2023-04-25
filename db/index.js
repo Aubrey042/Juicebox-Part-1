8c7b118284bfcc2fbe233f1d0c4c83746837577c
Aubrey042 <127706254+Aubrey042@users.noreply.github.com>
2023-04-25 06:32:40 -0400

    Index.js function order changed

--- db/index.js
+++ db/index.js
@@ -3,6 +3,21 @@ const { Client } = require('pg');
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5433/juicebox');
 
+
+async function createUser({ username, password }) {
+  const result = await client.query(`
+     INSERT INTO users(username, password) 
+     VALUES($1, $2) 
+     ON CONFLICT (username) DO NOTHING 
+     RETURNING *;
+   `, [username, password]);
+
+   return result;
+ }
+
+
+
+
 async function getAllUsers() {
   const { rows } = await client.query(`
     SELECT id, username 
@@ -12,20 +27,12 @@ async function getAllUsers() {
   return rows;
 }
 
-async function createUser({ username, password }) {
-   const result = await client.query(`
-      INSERT INTO users(username, password) 
-      VALUES($1, $2) 
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `, [username, password]);
 
-    return result;
-  }
 
 
 module.exports = {
   client,
+  createUser,
   getAllUsers,
-  createUser
+  
 };
